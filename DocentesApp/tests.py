30fe8ba14371fe2840f7ddc51e66d518b6fff8dd
Benjamín Caba <bca4ba@gmail.com>
2026-08-28 23:47:46 -0400

    Unifica diseño de docentes y estudiantes

--- DocentesApp/tests.py
+++ DocentesApp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class DocentesViewsTests(TestCase):
+    def test_login_docente_renderiza_la_base_compartida(self):
+        respuesta = self.client.get(reverse("login"))
+
+        self.assertContains(respuesta, "Colegio Digital")
+        self.assertContains(respuesta, "Área de Docentes")
+
+    def test_docente_puede_iniciar_sesion_y_ver_panel(self):
+        respuesta = self.client.post(
+            reverse("login"),
+            {"cuenta": "ejemplo@demo.cl", "rut": "12345678-9"},
+        )
+
+        self.assertRedirects(respuesta, reverse("listado_docentes"))
+        self.assertContains(self.client.get(reverse("listado_docentes")), "Panel de información académica")
