1272e360e2b519f236fa74e374810bdb9598764f
Jorge <jpaveza@gmail.com>
2026-08-28 20:58:41 -0400

    Jorge agrega modelo de AdministrativosApp y actualiza requirements.txt

---
+++ AdministrativosApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdministrativosappConfig(AppConfig):
+    name = 'AdministrativosApp'
--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'c:\django\colegio\venv'
+set -gx VIRTUAL_ENV 'C:\django\colegio\ES1_Backend\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
